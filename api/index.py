6918a7e1b15a11e7da594414a381e6d337686623
Michel Metran <michelmetran@gmail.com>
2022-05-14 18:52:16 -0300

    AAAA

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import numpy as np
 
 
 class handler(BaseHTTPRequestHandler):
@@ -9,9 +10,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+        a = str(np.random.choice([1, 2, 3, 4, 5, 6]))
         if 'name' in dic:
             message = 'Hello, ' + dic["name"] + '!'
         else:
-            message = 'Hello, stranger!'
+            message = f'Hello, stranger! {a}'
             self.wfile.write(message.encode())
         return
